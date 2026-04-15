6bdf04376637ea5347446f6e4eb64ba91c0c73a9
Mithun <mithun.s2004tumkur@gmail.com>
2026-04-15 11:43:16 +0530

    Deploy mithun_rocks web to gh-pages

--- android/app/src/main/kotlin/com/mithun/mithun_rocks/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.mithun.mithun_rocks
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:mithun_rocks/main.dart';
-
-void main() {
-  testWidgets('App smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MithunRocksApp());
-  });
-}
